a47bbdefee2a5480a1b82b8e42399b7b78f9c285
Kritsada Tavarasumida <kritsada.tavarasumida@gmail.com>
2016-05-15 16:32:28 +0700

    V 0.2 - regenerate materialize.min.css V 0.2.1 - add app.js - add onesignal api V 0.2.1-1 - modify config.xml V 0.2.1-2 - added code in home - partially V 0.2.1-3 - added code in product - partially V 0.2.1-4 - added code in product - Done

--- www/js/app.js
+++ www/js/app.js
@@ -25,11 +25,20 @@ if (page_name == 'product') {
     var page_live_catalog = 1;
     var formData = "pid=" + page_live_catalog;
     $.ajax({
-        url: serverURL + "get_icons_by_pid.php",
+        url: serverURL + "get-icons-by-pid.php",
         type: "POST",
         data: formData,
         success: function (data, textStatus, jqXHR) {
             console.log(data);
+            console.log(data.data.length);
+            var html = "";
+            for (var i = 0; i < data.data.length; i++) {
+                html = "";
+                html = '<div class="product half">';
+                html += '<a href="product-mock.html"><img src="' + data.data[i].url.replace(/%3A/g, ':').replace(/%2F/g, '/') + '" alt=""></a>';
+                html += '</div>';
+                $('#product3').append(html);
+            }
             //data - response from server
 
 
