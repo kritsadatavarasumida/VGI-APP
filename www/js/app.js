3c8467d405aa5e009a42ea59f946132e2fb8a9d0
Kritsada Tavarasumida <kritsada.tavarasumida@gmail.com>
2016-05-15 17:54:44 +0700

    V 0.2 - regenerate materialize.min.css V 0.2.1 - add app.js - add onesignal api V 0.2.1-1 - modify config.xml V 0.2.1-2 - added code in home - partially V 0.2.1-3 - added code in product - partially V 0.2.1-4 - added code in product - Done V 0.2.1-5 - added code in UI struture

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,18 @@
 /**
  * Created by doi on 5/15/2016 AD.
  */
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
 if (page_name == 'home') {
     var formData = "";
     $.ajax({
@@ -29,13 +41,13 @@ if (page_name == 'product') {
         type: "POST",
         data: formData,
         success: function (data, textStatus, jqXHR) {
-            console.log(data);
-            console.log(data.data.length);
+            //console.log(data);
+            //console.log(data.data.length);
             var html = "";
             for (var i = 0; i < data.data.length; i++) {
                 html = "";
                 html = '<div class="product half">';
-                html += '<a href="product-mock.html"><img src="' + data.data[i].url.replace(/%3A/g, ':').replace(/%2F/g, '/') + '" alt=""></a>';
+                html += '<a href="' + data.data[i].linkto + '.html?pid=' + data.data[i].next_page + '"><img src="' + data.data[i].url.replace(/%3A/g, ':').replace(/%2F/g, '/') + '" alt=""></a>';
                 html += '</div>';
                 $('#product3').append(html);
             }
@@ -47,5 +59,66 @@ if (page_name == 'product') {
             console.log(jqXHR);
 
         }
+    });
+}
+
+if (page_name == 'list') {
+    var param1 = getUrlVars()["pid"];
+    var formData = "pid=" + param1;
+    $.ajax({
+        url: serverURL + "get-icons-by-pid.php",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            //console.log(data);
+            //console.log(data.data.length);
+            var html = "";
+            for (var i = 0; i < data.data.length; i++) {
+                html = "";
+                html = '<div class="product half">';
+                html += '<a href="' + data.data[i].linkto + '.html?pid=' + data.data[i].next_page + '"><img src="' + data.data[i].url.replace(/%3A/g, ':').replace(/%2F/g, '/') + '" alt=""></a>';
+                html += '</div>';
+                $('#product3').append(html);
+            }
+            //data - response from server
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+
+        }
+    });
+
+    $.ajax({
+        url: serverURL + "get-banners-by-pid.php",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            //console.log(data);
+            //console.log(data.data.length);
+            var html = "";
+            for (var i = 0; i < data.data.length; i++) {
+                html = "";
+                html = '<div class="swiper-slide">';
+                html += '<img src="' + data.data[i].url.replace(/%3A/g, ':').replace(/%2F/g, '/') + '" alt=""></a>';
+                html += '</div>';
+                $('#banners').append(html);
+            }
+            //data - response from server
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+
+        }
+    });
+
+}
+
+if (page_name == "vdo") {
+    $('#btn-back').on('click', function () {
+        window.history.go(-1);
     });
 }
