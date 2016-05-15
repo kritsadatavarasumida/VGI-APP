9f6ca936677ea308accc60c75fc8876250c19e69
Kritsada Tavarasumida <kritsada.tavarasumida@gmail.com>
2016-05-15 16:06:28 +0700

    V 0.2 - regenerate materialize.min.css V 0.2.1 - add app.js - add onesignal api V 0.2.1-1 - modify config.xml V 0.2.1-2 - added code in home - partially V 0.2.1-3 - added code in product - partially

--- www/js/app.js
+++ www/js/app.js
@@ -2,13 +2,36 @@
  * Created by doi on 5/15/2016 AD.
  */
 if (page_name == 'home') {
+    var formData = "";
     $.ajax({
         url: serverURL + "news-home.php",
         type: "POST",
         data: formData,
         success: function (data, textStatus, jqXHR) {
-            console.log(JSON.stringify(data.data));
+            console.log(data);
             //data - response from server
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+
+        }
+    });
+}
+
+if (page_name == 'product') {
+    amplitude.logEvent('Enter Live Catalog');
+    var page_live_catalog = 1;
+    var formData = "pid=" + page_live_catalog;
+    $.ajax({
+        url: serverURL + "get_icons_by_pid.php",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+            //data - response from server
+
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
